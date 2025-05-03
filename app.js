76366fcd08e80d4c81691dbd5eeea7187dc73a06
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-03 20:05:14 +0200

    improve console.log for clarity

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const section = document.getElementById('main-list')
-console.log(section);
-console.dir(section);
+console.log('section', section);
+console.dir('section', section);
 
 const listItems = document.getElementsByClassName('list-item')
+const listItemsFirst = listItems[0]
 
-console.log(listItems);
+console.log('listItems', listItems);
+console.log('listItemsFirst', listItemsFirst);
 
 for(let i=0; i<listItems.length; i++){
-    console.log(listItems[i]);
+    console.log('listItems' + (i +1) + listItems[i]);
 }
 
 const listItemsByTagName = document.getElementsByTagName('li')
 
-console.log(listItemsByTagName);
+console.log('listItemsByTagName', listItemsByTagName);
