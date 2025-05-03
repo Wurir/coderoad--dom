56e345609dcef76d3086ae313851eedadb66960b
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-03 20:16:15 +0200

    querySelectorAll

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const listItem = document.querySelector('.list-item')
 // querySelector wybiera pierwszy item z podana klasa, tagiem itp
 console.log('list-item', listItem);
 console.dir(listItem);
+
+const listItems = document.querySelectorAll('.list-item')
+// querySelector wybiera wszystkie elementy
+console.log('listItems', listItems);
+console.dir(listItems);
