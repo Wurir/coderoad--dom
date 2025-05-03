d2a0f82b579f170230a01737b04baa1870fa897f
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-03 20:29:42 +0200

    innerText

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-const listItem = document.querySelector('.list-item')
-// querySelector wybiera pierwszy item z podana klasa, tagiem itp
-console.log('list-item', listItem);
-console.dir(listItem);
+const userName = prompt('Give me your name')
 
-const listItems = document.querySelectorAll('.list-item')
-// querySelector wybiera wszystkie elementy
-console.log('listItems', listItems);
-console.dir(listItems);
+const h1 = document.querySelector('h1')
 
-const secondaryListItems = document.querySelectorAll('#secondary-list .list-item')
-
-console.log('secondaryListItems', secondaryListItems);
-console.dir(secondaryListItems);
-
-const mainList = document.querySelector('#main-list')
-
-const mainListItems = mainList.querySelectorAll('.list-item')
-console.log('mainListItems', mainListItems);
-
-for(let i=0; i<mainListItems.length; i++){
-    console.log('mainListItems loop', mainListItems[i])
-}
+h1.innerText = 'Welcome ' + userName
