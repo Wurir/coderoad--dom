bf439d0711a93b923270dea31c04a4605971fc3b
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-03 20:51:52 +0200

    innerHtml

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const userName = 'Bartek'
 const h1 = document.querySelector('h1')
 
 h1.innerText = 'Welcome ' + userName
+
+document.body.innerHTML = '<p>Cześć Bartek</p>'
+// to nadpisze cale body
