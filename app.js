e92ef1edf1e5e9e7669b8a5ba46f7f11ce3c3b82
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-03 20:01:48 +0200

    getElementsByClassName

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const section = document.getElementById('main-list')
 console.log(section);
 console.dir(section);
+
+const listItems = document.getElementsByClassName('list-item')
+
+console.log(listItems);
+
+for(let i=0; i<listItems.length; i++){
+    console.log(listItems[i]);
+}
