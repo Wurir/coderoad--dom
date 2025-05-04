cd2d2ee2ad3cd3abdb8c8a88c21af1a9cb2ddc91
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-04 22:04:55 +0200

    createTextNode

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const body = document.querySelector('body')
 const input = document.createElement('input')
+const text = document.createTextNode('I am a text node')
 
 body.appendChild(input)
+body.appendChild(text)
