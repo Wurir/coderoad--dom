02dce9357d0a09adf4eeb8cd3550d0d0f26c537b
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-04 22:05:44 +0200

    adding <br>

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const body = document.querySelector('body')
 const input = document.createElement('input')
 const text = document.createTextNode('I am a text node')
+const br = document.createElement('br')
 
 body.appendChild(input)
+body.appendChild(br)
 body.appendChild(text)
