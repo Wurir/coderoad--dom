b4845d1bfe6af8e49bef4dfdef3d70f22e7a388f
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-04 22:22:11 +0200

    prepend

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const p = document.createElement('p')
 p.className = 'paragraph fourth'
 p.innerHTML = 'Fourth paragraph'
 
-container.append(p)
-// append is identical to appendChild but is not compatible with all browsers
+container.prepend(p)
+// prepend is adding new element as a 1st child
