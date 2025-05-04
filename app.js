389d7d305fbf34f5fd7892458b6fa0f4afd16ede
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-04 22:21:08 +0200

    append and appendChild

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ const p = document.createElement('p')
 p.className = 'paragraph fourth'
 p.innerHTML = 'Fourth paragraph'
 
-container.appendChild(p)
+container.append(p)
+// append is identical to appendChild but is not compatible with all browsers
