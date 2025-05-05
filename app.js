9610b31d369e4c0230a9f0f0862f72eb0e0de10c
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-05 16:03:32 +0200

    insertBefore()

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const p = document.createElement('p')
 p.className = 'paragraph fourth'
 p.innerHTML = 'Fourth paragraph'
 
-container.prepend(p)
-// prepend is adding new element as a 1st child
+container.insertBefore(p, document.querySelector('.second'))
+// pierwszy argument to element ktory ma zostac dodany, drugi argument to element przed ktorym ma zostac dodany. 
+// Wpisujac null, dodam na koncu
