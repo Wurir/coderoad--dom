c0f0b503bd445dbc1c589145e08a4ad3cc059a0f
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-05 16:32:26 +0200

    styles using obj

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const square = document.querySelector('.square')
 
-square.setAttribute('style', 'background-color: red; width: 100px; height: 100px')
+// square.setAttribute('style', 'background-color: red; width: 100px; height: 100px')
+
+square.style.width = '100px'
+square.style.height = '100px'
+square.style.backgroundColor = 'red'
