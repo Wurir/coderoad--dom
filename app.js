affb81a06d23113b366489d80dd25104cb7feb66
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-05 16:10:10 +0200

    firstElementChild /  lastElementChild

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const p = document.createElement('p')
 p.className = 'paragraph fourth'
 p.innerHTML = 'Fourth paragraph'
 
-container.insertBefore(p, document.querySelector('.second'))
+container.insertBefore(p, container.lastElementChild) // firstElementChild, wstawi przed pierwszym el w container
 // pierwszy argument to element ktory ma zostac dodany, drugi argument to element przed ktorym ma zostac dodany. 
 // Wpisujac null, dodam na koncu
