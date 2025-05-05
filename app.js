240a67404394f1f6c10e07b5d3ab74d4a5613459
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-05 16:11:59 +0200

    fn to add elements to DOM

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const p = document.createElement('p')
 p.className = 'paragraph fourth'
 p.innerHTML = 'Fourth paragraph'
 
-container.insertBefore(p, container.lastElementChild) // firstElementChild, wstawi przed pierwszym el w container
-// pierwszy argument to element ktory ma zostac dodany, drugi argument to element przed ktorym ma zostac dodany. 
-// Wpisujac null, dodam na koncu
+prepend(container, p)
+
+function prepend(container, element){
+    container.insertBefore(element, container.firstElementChild) 
+}
